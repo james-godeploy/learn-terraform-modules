1b55a6fd084142fed01dae73bc684c7a5a960a47
james-godeploy <109163103+james-godeploy@users.noreply.github.com>
2022-07-12 17:12:29 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami               = "ami-01cc34ab2709337aa"
+  instance_type     = "t2.micro"
+  subnet_id = aws_subnet.my_subnet.id
+}
